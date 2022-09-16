ae4263b31b600da20080d2df90eaed3f94fd622f
HyeongHwan <gudghks56@gmail.com>
2022-09-16 16:41:37 +0900

    navigation setting

--- CatServiceProject/SceneDelegate.swift
+++ CatServiceProject/SceneDelegate.swift
@@ -11,42 +11,45 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions)  {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.makeKeyAndVisible()
+        
+        let tabBarController = UITabBarController()
+        
+        
+        let catListViewController = CatMainListViewController()
+        let mainViewController = CatBreedsListViewController()
+        let favoriteviewController = FavouriteViewController()
+        let catupLoadViewController = CatUploadViewController()
+        
+        let mainCatListViewController = UINavigationController(rootViewController: catListViewController)
+        
+        let favoriteNavigationViewController = UINavigationController(rootViewController: mainViewController)
+        
+        
+        let catUpLoadNavigationViewController = UINavigationController(rootViewController: catupLoadViewController)
+     
+        catListViewController.tabBarItem = UITabBarItem(title: "app", image: UIImage(systemName: "app"), selectedImage: UIImage(systemName: "app.fill"))
+        
+        favoriteNavigationViewController.tabBarItem = UITabBarItem(title: "Breeds", image: UIImage(systemName: "timer.square"), selectedImage: UIImage(systemName: "timer.square"))
+        
+        favoriteviewController.tabBarItem = UITabBarItem(title: "Favorite", image: UIImage(systemName: "star"), selectedImage: UIImage(systemName: "star.fill"))
+        
+        catUpLoadNavigationViewController.tabBarItem = UITabBarItem(title: "app", image: UIImage(systemName: "app"), selectedImage: UIImage(systemName: "app.fill"))
+        
+        tabBarController.setViewControllers([mainCatListViewController,favoriteviewController,favoriteNavigationViewController,catUpLoadNavigationViewController], animated: true)
+        
+        tabBarController.tabBar.tintColor = .white
+        tabBarController.tabBar.barTintColor = .systemGreen
+        tabBarController.tabBar.backgroundColor = .systemGreen
+        
+        window.rootViewController = tabBarController
+        
+        self.window = window
+        
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
+    
 }
 
